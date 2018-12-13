060fa3d9f8a1d628d27ac5266186d7a0ae8c1655
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-12-13 13:27:05 +0900

    active_admin install

---
+++ test/models/admin_user_test.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: admin_users
+#
+#  id                     :bigint(8)        not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_admin_users_on_email                 (email) UNIQUE
+#  index_admin_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
+require 'test_helper'
+
+class AdminUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
