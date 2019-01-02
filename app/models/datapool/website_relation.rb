cac7540a85f15d2f17629c4bf4026f87e0e8d37a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-01-02 09:22:45 +0900

    index追加

--- app/models/datapool/website_relation.rb
+++ app/models/datapool/website_relation.rb
@@ -7,6 +7,12 @@
 #  child_website_id  :integer          not null
 #  host_url          :string(255)      not null
 #
+# Indexes
+#
+#  index_datapool_website_relations_on_child_website_id   (child_website_id)
+#  index_datapool_website_relations_on_host_url           (host_url)
+#  index_datapool_website_relations_on_parent_website_id  (parent_website_id)
+#
 
 class Datapool::WebsiteRelation < ApplicationRecord
 end
--- db/migrate/20181230181039_create_datapool_website_relations.rb
+++ db/migrate/20181230181039_create_datapool_website_relations.rb
@@ -5,5 +5,8 @@ class CreateDatapoolWebsiteRelations < ActiveRecord::Migration[5.2]
       t.integer :child_website_id, null: false
       t.string :host_url, null: false
     end
+    add_index :datapool_website_relations, :parent_website_id
+    add_index :datapool_website_relations, :child_website_id
+    add_index :datapool_website_relations, :host_url
   end
 end
