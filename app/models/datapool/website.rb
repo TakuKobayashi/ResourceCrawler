1e09c4c56380fcc477d1cf44b7bd9bc93889f179
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-01-02 09:23:42 +0900

    特別な取得方法

--- app/models/datapool/website.rb
+++ app/models/datapool/website.rb
@@ -52,26 +52,56 @@ class Datapool::Website < Datapool::ResourceBase
     return website
   end
 
-  def scraping_html_recourses!
-    html_dom = RequestParser.request_and_parse_html(url: self.src.to_s, options: {:follow_redirect => true})
-    #docs = HTMLDom.doc_nodes(html_dom.children)
-    #attr_values = docs.map{|d| d.attributes.values }.flatten
+  @html_dom_cache = nil
 
+  def get_html_dom
+    @html_dom_cache ||= RequestParser.request_and_parse_html(url: self.src.to_s, options: {:follow_redirect => true})
+    return @html_dom_cache
+  end
+
+  def get_css_js_file_urls
+    html_dom = self.get_html_dom
     css_js_extes = Datapool::WebMetum::CSS_FILE_EXTENSIONS + Datapool::WebMetum::JS_FILE_EXTENSIONS
     css_js_file_urls = Sanitizer.scan_url_path_resources(html_dom.to_html.downcase, css_js_extes)
+    return css_js_file_urls
+  end
+
+  def scrape_cycle_links!
+    html_dom = self.get_html_dom
+    urls = html_dom.css("a").map{|atag| atag[:href]}
+    src_website = Datapool::Website.find_by_url(url: urls).index_by(&:src)
+    new_urls = urls.select{|url| src_website[url].present? }
+    websites = new_urls.map do |url|
+      ws = Datapool::Website.new(crawl_state: :cycle_crawling)
+      ws.src = url
+      ws
+    end
+    self.transaction do
+      Datapool::Website.import(websites)
+      self.update!(last_crawl_time: Time.current, crawl_state: :cycle_crawled)
+    end
+  end
+
+  def scrape_single_html_recourses!
+    html_dom = self.get_html_dom
+    #docs = HTMLDom.doc_nodes(html_dom.children)
+    #attr_values = docs.map{|d| d.attributes.values }.flatten
+    css_js_file_urls = self.get_css_js_file_urls
+
     contents = Sanitizer.scan_url_path_resources(html_dom.to_html.downcase, Datapool::ResourceMetum.resource_file_extensions)
     css_js_file_urls.each do |url|
       text = RequestParser.request_and_response_body(url: self.src.to_s, options: {:follow_redirect => true})
       contents += Sanitizer.scan_url_path_resources(text.downcase, Datapool::ResourceMetum.resource_file_extensions)
     end
     resource_meta = contents.uniq.map do |url|
-      Datapool::WebsiteResourceMetum.constract(
+      resource = Datapool::WebsiteResourceMetum.constract(
         url: url,
         title: self.title,
-        website_id: self.id,
         check_file: true,
         options: {}
       )
+      resource.website_id = self.id
+      resource
     end
     self.transaction do
       Datapool::WebsiteResourceMetum.import_resources!(resources: resource_meta)
