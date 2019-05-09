b0320ad09611705504a9731b0f583f3952c1d4ae
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-05-10 04:07:49 +0900

    各種環境作成

---
+++ lambda_functions/config/lambdaRoutes.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projectToRoot: {
+    'resource_crawler_flickr_search_resources_api': './lambda/flickr/search/resources/',
+    'resource_crawler_youtube_search_videos_api': './lambda/youtube/search/videos/',
+    'resource_crawler_youtube_video_api': './lambda/youtube/video/',
+    'resource_crawler_youtube_search_relations_api': './lambda/youtube/search/relations/',
+    'resource_crawler_google_search_links_url_api': './lambda/google/search/links/',
+  },
+}
--- lambda_functions/lambda/google/search/videos/index.js
+++ lambda_functions/lambda/google/search/videos/index.js
@@ -1,8 +1,9 @@
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
+    console.log(event);
     // TODO implement
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
     };
-    return response;
+    return event;
 };
