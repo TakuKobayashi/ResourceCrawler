44c98f8a95e412eceffc80a7f755308baf276a83
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-05-26 01:01:34 +0900

    更新

--- lambda_functions/lambdaBuild.js
+++ lambda_functions/lambdaBuild.js
@@ -3,9 +3,12 @@ const path = require('path');
 const archiver = require('archiver');
 const glob = require("glob");
 
+const requireRoot = require('app-root-path');
+const lambdaRoutes = require(requireRoot + "/config/lambdaRoutes");
+
 const archiveCompresser = function (platformName, projectName) {
   // create a file to stream archive data to.
-  const output = fs.createWriteStream(__dirname + '/' + [platformName, projectName].join('_') + '.zip');
+  const output = fs.createWriteStream(platformName + '.zip');
   const archive = archiver('zip');
   output.on('close', () => {
     console.log(archive.pointer() + ' total bytes');
@@ -21,7 +24,7 @@ const archiveCompresser = function (platformName, projectName) {
     throw err;
   });
   archive.pipe(output);
-  const filePathes = glob.sync('./lambda/' + platformName + '/' + projectName + '/**/*.js');
+  const filePathes = glob.sync(projectName + '/**/*.js');
   for (const filePath of filePathes) {
     const fileName = path.basename(filePath);
     archive.append(fs.createReadStream(filePath), {
@@ -29,15 +32,14 @@ const archiveCompresser = function (platformName, projectName) {
     });
   }
 
-  //archive.glob('./lambda/' + platformName + '/' + projectName + '/**/*.js');
-  archive.glob('./libs/**/*.js');
-  archive.glob('node_modules/**/');
+  archive.glob(projectName + '/**/*.js');
+  archive.glob(requireRoot + '/libs/**/*.js');
+  archive.glob(requireRoot + '/node_modules/**/');
   archive.finalize();
 }
 
-const platformNames = ['LineBot', 'FacebookBot'];
-for (const plName of platformNames) {
-  const projectPathes = glob.sync('./lambda/' + plName + '/*');
+for (const plName of Object.keys(lambdaRoutes.projectToRoot)) {
+  const projectPathes = glob.sync(requireRoot + lambdaRoutes.projectToRoot[plName]);
   for (const projectPath of projectPathes) {
     const projectName = path.basename(projectPath);
     archiveCompresser(plName, projectName);
